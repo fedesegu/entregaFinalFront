cf7d59640ec5f32659e4b6eb1ac92bcdafb4b5b4
Federico Segú <federicosegu@gmail.com>
2023-08-20 19:46:08 -0300

    agregando sweetalert

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,12 +1,3 @@
-//Aclaraciones de consignas requeridas: 
-// 1) Arregle el boton de resetear;  2) Le sume la funcion mostrar error (usando el operador ternario ? y :, que tambien es consigna) para que se tenga que ingresar valores mayores a 1; 3) le agregue un spread operator en la funcion contador; 4)agregue una funcion asincrona; 5) Agregue boton para sumar edades. 6) Agregue destructuracion con el id (que despues hay que reemplazarlo por edades de api), en la funcion asincrona
-
-//No consigna pero buena practica: las variables relacionadas con elementos del DOM (como las vinculadas a botones, se inician con $)
-
-//Faltaria como consigna:
-// Consumir alguna libreria como Sweet alert capaz para el error 
-
-
 let edad;
 let edades = [];
 let arraypersonas = [];
@@ -30,17 +21,13 @@ function contador (x) {
       $promediovalue.innerHTML = (Math.ceil(promedio)) + " Es la edad promedio entre todos los resultados";
 }
 
-
 let $submitButton = document.getElementById('button1');
 
-
-    
     function agregarEdadManual(){
     $submitButton.addEventListener('click', function(e) {
       
-      
       let $boxvalue = document.getElementById('edad').value;
-      $boxvalue = "" ? mostrarError ("Tienes que ingresar un número positivo mayor a 1") : isNaN ($boxvalue) ? mostrarError ("Tienes que ingresar un número positivo mayor a 1") : $boxvalue < 1 ? mostrarError("Tienes que ingresar un número positivo mayor a 1") : edades.push($boxvalue);
+      $boxvalue = "" ? mostrarError ("Tienes que ingresar un número positivo mayor o igual a 1") : isNaN ($boxvalue) ? mostrarError ("Tienes que ingresar un número positivo mayor o igual a 1") : $boxvalue < 1 ? mostrarError("Tienes que ingresar un número positivo mayor o igual a 1") : edades.push($boxvalue);
 
       contador(1);
      
@@ -62,17 +49,17 @@ edades = [];
 agregarEdadManual(edad);
 
 const mostrarError = (error) => {
-    const $mensajeError = document.createElement("p")
-    $mensajeError.textContent = error
-    $mensajeError.classList.add("error")
-      
-    const $contenido = document.getElementById("button1")
-    $contenido.appendChild($mensajeError) //<p> es hijo de ese contenido 
-    
-    setTimeout(() =>{
-        $mensajeError.remove() //para borrarlo del DOM 
-    }, 3000)
-  }
+    Swal.fire({
+      icon: 'error',
+      title: 'Error',
+      text: error,
+      timer: 3000,
+      timerProgressBar: true,
+      showConfirmButton: false,
+      allowOutsideClick: false,
+    })
+  
+}
 let $edadesApi = document.getElementById('button2')
 
 const api = async () =>{
@@ -86,14 +73,4 @@ const api = async () =>{
   })
  })
 }
-// }
 api();
-// console.log(edadesApi)
-
-// let sumarEdadesApi = (edadesApi) => {
-// edadesApi.reduce((a, b) => Number(a) + Number(b), 0);
- 
-// }
-
-// console.log(sumarEdadesApi(edadesApi));
-// habria que meter algo de spread operator
